4065a4b5d4ce5995ba2a572ee02f65fee773df6c
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-29 22:26:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ bot.on("message", async message => {
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
 
+  if(!message.content.startsWith(prefix)) return;
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
