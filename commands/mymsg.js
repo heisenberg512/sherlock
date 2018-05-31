7ee673377718420526ad56b85768e07d5d59b696
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-31 20:11:03 +0500

    Update mymsg.js

--- commands/mymsg.js
+++ commands/mymsg.js
@@ -1,14 +1,11 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You can't do that pal").then(msg => msg.delete(5000));
-  if(message.author.id === "329119469509148683") return message.reply("You are nub GTFO").then(msg => msg.delete(5000))
+  if(!message.author.id === "332134348725813248") return;
   let msg = args.join(" ");
-  message.delete();
+  message.delete().catch(O_o => {});
   message.channel.send(msg);
 
-//   let pmChannel = message.guild.channels.find(`name`, "terrorists-camp");
-// pmChannel.send(msg);
 }
 
 module.exports.help = {
