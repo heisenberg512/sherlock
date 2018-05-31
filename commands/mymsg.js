742610390fad3d405910e8212250b5a0551ba5e6
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-31 22:58:43 +0500

    Update mymsg.js

--- commands/mymsg.js
+++ commands/mymsg.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.author.id === "332134348725813248") return;
+  if(!message.author.id === 332134348725813248) return;
   let msg = args.join(" ");
   message.delete().catch(O_o => {});
   message.channel.send(msg);
