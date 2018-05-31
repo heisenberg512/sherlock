ba3889ebce626fe0dd191d3042415b8d0ca1873f
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-31 22:52:19 +0500

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -7,6 +7,7 @@ module.exports.run = async (bot, message, args) => {
   .addField("doggy", "sends random picture of doggy everytime user enters the command")
   .addField("avatar", "sends the avatar of author or the user tagged infront of it")
   .addField("info", "displays information about the bot.")
+  .addField("tempmute", "mute members for a limited time")
   .setColor(botconfig.purple);
 
   message.delete().catch(O_o => {});
