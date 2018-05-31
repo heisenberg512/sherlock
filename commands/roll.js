ad7d6039c036ab38e7660f9771e04b04cd197e3b
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-31 20:12:32 +0500

    Update roll.js

--- commands/roll.js
+++ commands/roll.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 
 module.exports.run = async (bot, message, args) =>{
-//   let terr = message.guild.roles.find("name", "Terrorists");
-// if(!message.member.roles.has(terr.id)) return message.channel.send("You are not a terrorists member");
+  let terr = message.guild.roles.find("name", "Member");
+if(!message.member.roles.has(terr.id)) return;
   var roll = Math.floor(Math.random() * 6) + 1;
   message.reply("You rolled a " + roll);
   message.delete().catch(O_o=>{});
