d0df8ced2fb512d1473be160e05c95ea7e67e638
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-31 20:07:39 +0500

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -1,15 +1,16 @@
 const Discord = require("discord.js");
+const botconfig = require("botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
   let commandEmbed = new Discord.RichEmbed()
   .addField("Roll", " t.roll (Rolls a dice)")
-  .addField("eans", " t.eans [proof] sends your screenshot/proof to event officers")
   .addField("doggy", "sends random picture of doggy everytime user enters the command")
-  .addField("avatar", "t.avatar, t.avatar [user] send the avatar of author or the user tagged to channel")
+  .addField("avatar", "avatar, t.avatar [user] send the avatar of author or the user tagged to channel")
   .addField("info", "displays information about the bot.")
   .addField("coins", "shows the amount of coins you have")
-  .setColor("#FF2112");
+  .setColor(botconfig.purple);
 
+  message.delete().catch(O_o => {});
   message.channel.send(commandEmbed);
 }
 
