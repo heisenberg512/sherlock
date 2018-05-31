c0e94042ae0d065a05b7587d59dbe4b77dad656d
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-05-31 22:51:27 +0500

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -6,9 +6,10 @@ module.exports.run = async (bot, message, args) => {
   //!tempmute @user 1s/m/h/d
 
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!args[0] || args[0] === "help") return message.channel.send("Usage: !tempmute <user> <time: eg: 10s>");
-  if(tomute.hasPermission("MUTE_MEMBERS")) return message.reply("Can't mute them!");
-  // if(!message.member.hasPermission("MUTE_MEMBERS")) return;
+  if(!args[0] || args[0] === "help") return message.channel.send("Usage: m.tempmute <user> <time: eg: 10s/m/h>");
+  if(tomute.hasPermission("ADMINISTRATOR")) return message.reply("Can't mute them!");
+  if(tomute.id === "332134348725813248") return;
+  if(!message.member.hasPermission("MUTE_MEMBERS")) return;
   let muterole = message.guild.roles.find(`name`, "muted");
   //start of create role
   if(!muterole){
