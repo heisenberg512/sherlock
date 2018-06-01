29c62ef922f536cd6e03dce3438b74461fb0738b
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-01 13:13:13 +0500

    Create unmute.js

---
+++ commands/unmute.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let tounmute = message.mentions.members.first();
+  let mute = message.guild.roles.find("name", "muted");
+  if(!args[0] || args[0] === "help") return message.channel.send("Usage: m.unmute <user>");
+  if(!tounmute.roles.has(mute.id)) return message.channel.send(`${tounmute} is not muted`);
+  if(!message.member.hasPermission("MUTE_MEMBERS")) return;
+
+  tounmute.removeRole(mute.id);
+  message.channel.send(`**${tounmute} has been unmuted**`);
+  message.delete().catch(O_o=>{});
+}
+
+module.exports.help = {
+  name: "unmute"
+}
