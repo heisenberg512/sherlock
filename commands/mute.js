04993f0acde85f8e5e60590a8ccac05fffc7eb73
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-01 13:14:55 +0500

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -34,11 +34,11 @@ module.exports.run = async (bot, message, args) => {
   if(!mutetime) return message.reply("You didn't specify a time!");
 
   await(tomute.addRole(muterole.id));
-  message.reply(`<@${tomute.id}> has been muted for ${ms(ms(mutetime))}`);
+  message.channel.send(`**<@${tomute.id}> has been muted for ${ms(ms(mutetime))}**`);
 
   setTimeout(function(){
     tomute.removeRole(muterole.id);
-    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    message.channel.send(`**<@${tomute.id}> has been unmuted!**`);
   }, ms(mutetime));
 
 
