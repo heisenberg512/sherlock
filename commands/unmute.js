04eb8bfdbd5c0a69bc8a688a748d11cb0bd6af03
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-03 01:16:46 +0500

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let tounmute = message.mentions.members.first();
   let mute = message.guild.roles.find("name", "muted");
-  if(!args[0] || args[0] === "help") return message.channel.send("Usage: m.unmute <user>");
+  if(!args[0] || args[0] === "help") return message.channel.send("Usage: $unmute <user>");
   if(!tounmute.roles.has(mute.id)) return message.channel.send(`${tounmute} is not muted`);
   if(!message.member.hasPermission("MUTE_MEMBERS")) return;
 
