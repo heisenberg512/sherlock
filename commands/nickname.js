1de541f4b83d74fe0e586b7dd8877e0e318ac7c8
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-03 01:14:57 +0500

    Update nickname.js

--- commands/nickname.js
+++ commands/nickname.js
@@ -4,19 +4,17 @@ const botconfig = require("../botconfig.json");
 module.exports.run = async (bot, message, args) => {
   let nuser = message.mentions.users.first();
   let nick = args.slice(1).join(" ");
- if(!args[0] || args[0] === "help") return message.reply(`Usage: m.nickname <user> <nick>`);
-  let organizer = message.guild.roles.find("name", "Leader");
-  if(!message.member.roles.has(organizer.id)) return;
+ if(!args[0] || args[0] === "help") return message.reply(`Usage: $nickname <user> <nick>`);
+if(message.author.hasPermission("MANAGE_SERVER")) return;
 
-   message.delete();
+   message.delete().catch(O_o=>{});
    message.guild.member(nuser).setNickname(nick);
 
    let nEmbed = new Discord.RichEmbed()
    .setTitle("Nickname")
    .setTimestamp()
    .setColor(botconfig.yellow)
-   .setDescription(`${nuser}'s nick has been changed to ${nick}`)
-   .setFooter("Bunny: Get rekt", nuser.avatarURL);
+   .setDescription(`${nuser}'s nick has been changed to ${nick}`);
 
  message.channel.send(nEmbed).then(msg => msg.delete(10000));
 
