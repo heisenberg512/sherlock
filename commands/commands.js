0c89ea5d27e729ad2e8d8c16d631c4e864ce8056
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-04 00:04:06 +0500

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -3,12 +3,14 @@ const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
   let commandEmbed = new Discord.RichEmbed()
+  .setTitle("Commands")
   .addField("roll", "roll (Rolls a dice)")
   .addField("doggy", "sends random picture of doggy everytime user enters the command")
   .addField("avatar", "sends the avatar of author or the user tagged infront of it")
   .addField("info", "displays information about the bot.")
   .addField("stats", "displays the time when you joined M.B.R server")
   .addField("mute/unmute", "mute/unmute members")
+  .addField("ban/unban", "ban/unban users")
   .addField("weather", "shows the weather forcast of location entered")
   .setColor(botconfig.purple);
 
