eb066311bfb98ca634718ab42fd32e30e0d59011
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-03 20:49:32 +0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,26 +1,26 @@
 const Discord = require("discord.js");
 const botconfig = require("../botconfig.json");
 
-module.exports.run = async (bot, message, args) => {
-
-     let toban = message.mentions.members.first();
-     let reason = args.slice(1).join(" ");
-     if(!message.author.has("ADMINISTRATOR")) return message.channel.send("Permission Denied");
-     if(!args[0] || args[0] === "help") return message.channel.send("**Usage: ban <user> <reason>**");    
-     if(toban.has("ADMINISTRATOR")) return;
+module.exports.run = async(bot, message, args) => {
+let toban = message.members.mentions.first();
+     let reason  = args.slice(1).join(" ");
+     if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Permission Denied");
+     if(toban.hasPermission("ADMINISTRATOR")) return message.reply("I can't mute him");
+     if(!args[0] || args[0] === "help") return message.channel.send("Usage: ban <user> <reason>");
      
-     let bembed = new Discord.RichEmbed()
-     .setAuthor(`${toban} has been banned by ${message.auhtor.id}`)
-     .setDescription(`**${reason}**`)
+     let embed = new Discord.RichEmbed()
+     .setAuthor(`${toban.username} has been banned`)
+     .setDescription(reason)
+     .addField("Banned by", `${message.author.id}`)
      .setColor(botconfig.green)
-     .setFooter(tomute.avatarURL)
      .setTimestamp();
-
+     
      message.delete().catch(O_o=>{});
-     message.guild.member(toban).ban(reason);
-     toban.send(`**You have been banned reason ${reason}**`);
+     message.guild.member(toban.id).ban(reason);
+     message.channel.send(embed);
 }
 
-module.exports.help =  {
-    name: "ban"
+
+module.exports.help = {
+ name: "ban"
 }
