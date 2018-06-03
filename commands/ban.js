47bd61b3be66031130f2f21a5af71a0438bdae87
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-03 22:22:07 +0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,7 +4,7 @@ const botconfig = require("../botconfig.json");
 module.exports.run = async(bot, message, args) => {
 let toban = message.mentions.members.first();
      let reason  = args.slice(1).join(" ");
-//      if(!args[0] || args[0] === "help") return message.channel.send("Usage: ban <user> <reason>");
+     if(args[0] || args[0] === "help") return message.channel.send("Usage: ban <user> <reason>");
      if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Permission Denied");
      if(toban.hasPermission("ADMINISTRATOR")) return message.reply("I can't mute him");
 
