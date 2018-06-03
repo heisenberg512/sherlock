a331cf6a08d15009ae1e4bb51f7b2d88f805d997
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-04 01:18:36 +0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
   let user = message.mentions.members.first();
   
-  message.channel.createInvite().then( invite => user.send(`https://discord.gg/${invite}`));
+  message.channel.createInvite().then( invite => user.send(`https://discord.gg/${invite.code}`));
   
 
 }
