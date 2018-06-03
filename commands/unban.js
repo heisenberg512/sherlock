6054ae72439b129ebe045f25aacf84e18d6c24f0
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-04 00:46:42 +0500

    Create unban.js

---
+++ commands/unban.js
@@ -0,0 +1,23 @@
+  const Discord = require("discord.js");
+  
+  module.exports.run = async (bot, message, args) => {
+    let tounban = message.mentions.members.first();
+    if(!args[0] || args[0] === "help") return message.channel.send("Usage: unban <user>");
+  
+  let invite = message.channel.createInvite();
+  let embed = new Discord.RichEmbed();
+  .setTitle(Unban Report)
+  .addField("Unbanned user", `${tounban.user.username}`)
+  .addField("Unbanned by", message.author)
+  .setTimestamp();
+  
+  message.delete().catch(O_o=>{});
+  tounban.unban;
+  tounban.send(`**You have been unbanned from ${message.guild.name}**`);
+  tounban.send(invite);
+  }
+  
+  
+  module.exports.help = {
+  name: "unban"
+  }
