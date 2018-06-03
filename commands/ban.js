0cd4227c16c2c9e1e085a63f8cc54ed78a0be082
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-03 21:09:30 +0500

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,9 +4,10 @@ const botconfig = require("../botconfig.json");
 module.exports.run = async(bot, message, args) => {
 let toban = message.mentions.members.first();
      let reason  = args.slice(1).join(" ");
+     if(!args[0] || args[0] === "help") return message.channel.send("Usage: ban <user> <reason>");
      if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Permission Denied");
      if(toban.hasPermission("ADMINISTRATOR")) return message.reply("I can't mute him");
-     if(!args[0] || args[0] === "help") return message.channel.send("Usage: ban <user> <reason>");
+
      
      let embed = new Discord.RichEmbed()
      .setAuthor(`${toban.username} has been banned`)
