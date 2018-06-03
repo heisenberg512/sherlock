84a7ad9c000804e355daed88fb573fb2490dccd2
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-03 20:19:54 +0500

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+
+     if(tomute.hasPermisson("ADMINISTRATOR")) return;
+     if(!message.author.hasPermisson("ADMINISTRATOR")) return message.channel.send("Permission Denied");
+     if(!args[0] || args[0] === "help") return message.channel.send("**Usage: ban <user> <reason>**");    
+     let toban = message.mentions.members.first();
+     let reason = args.slice(1).join(" ");
+
+     let bembed = new Discord.RichEmbed()
+     .setAuthor(`${toban} has been banned by ${message.auhtor.id}`)
+     .setDescription(`**${reason}**`)
+     .setColor(botconfig.green)
+     .setFooter(tomute.avatarURL)
+     .setTimestamp();
+
+     message.delete().catch(O_o=>{});
+     message.guild.member(toban).ban(reason);
+     toban.send(`**You have been banned reason ${reason}**`);
+}
+
+module.exports.help =  {
+    name: "ban"
+}
