aa5805de51ea167547fde1e85826bfe52f0b8c7d
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-04 17:26:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ fs.readdir("./commands/", (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is now online`);
-  bot.user.setActivity("Sex", {type: "Having"});
+  bot.user.setActivity("with pussy", {type: "Playing"});
 });
 
 bot.on("message", async message => {
