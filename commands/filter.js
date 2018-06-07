b8c1f605985010a89ac75b74d7e58c8af57fe121
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-07 18:46:39 +0500

    Create filter.js

---
+++ commands/filter.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+modudle.exports.run = async (bot, message, args) {
+let forbidden = ["porn", "mc", "bc", "fuck, "fck","bsdk","bhonsrike","bhonsdike", "gandu", "gand", "danda", "lund"];
+const filer = message.content.includes(forbidden);
+const collector = message.channel.createMessageCollector(filter, {time: 300000});
+collector.on('colloect', m => message.channel.send(`Collocted ${m.content}`));
+collector.on('end', collected => message.channel.send(`Collected ${collected.size} items`));
+
+message.delete().catch(O_o=>{});
+
+}
+
+
+module.exports.help = {
+name: "filer"
+}
