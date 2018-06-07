1c6ef69da8ce3f677c4b0e96beac3d0a9722bb20
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-07 18:51:41 +0500

    Update filter.js

--- commands/filter.js
+++ commands/filter.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message, args) => {
 let forbidden = ["porn", "mc", "bc", "fuck", "fck","bsdk","bhonsrike","bhonsdike", "gandu", "gand", "danda", "lund"];
 const filter = message.content.includes(forbidden);
 const collector = message.channel.createMessageCollector(filter, {time: 300000});
-collector.on('colloect', m => message.channel.send(`Collocted ${m.content}`));
-collector.on('end', collected => message.channel.send(`Collected ${collected.size} items`));
+collector.on('colloect', m => console.log(`Collocted ${m.content}`));
+collector.on('end', collected => console.log(`Collected ${collected.size} items`));
 
 message.delete().catch(O_o=>{});
 
