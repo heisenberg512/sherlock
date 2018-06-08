0bff31c7bb0665a9e9a11dfa391eb971fac95b26
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-08 16:56:15 +0500

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
 
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   let reason = args.slice(1).join(" ");
-  f(!args[0] || args[0] === "help") return message.channel.send("Usage: $mute <user> <reason>");
+  if(!args[0] || args[0] === "help") return message.channel.send("Usage: $mute <user> <reason>");
   if(tomute.hasPermission("ADMINISTRATOR")) return message.reply("Can't mute them!");
   if(tomute.id === "332134348725813248") return;
   if(!message.member.hasPermission("MUTE_MEMBERS")) return;
