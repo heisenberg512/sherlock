d906618a382ef276fdb84ad039487f235ba01a02
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-09 22:22:17 +0500

    Update setnick.js

--- commands/setnick.js
+++ commands/setnick.js
@@ -3,7 +3,7 @@ const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
 if(!args[0]) return message.channel.send("Usage: mynick <desired nick>");
-let nick = args.slice(1).join(" ");
+let nick = args.join(" ");
 
 message.guild.member(message.author).setNickname(nick);
 message.reply(`Your nickname has been changed to ${nick}`);
