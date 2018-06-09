cff683aea4a76474a896e6dec9779ebde79accf7
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-09 22:18:04 +0500

    Create setnick.js

---
+++ commands/setnick.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+if(!args[0]) return message.channel.send("Usage: mynick <desired nick>");
+let nick = args.slice(1).join(" ");
+
+message.guild.author.setNickname(nick);
+message.reply(`Your nickname has been changed to ${nick}`);
+
+}
+
+
+module.exports.help = {
+ name: "mynick"
+}
