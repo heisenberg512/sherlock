26500bc80f348c33ea592f6aa704ab6a1fae4ae6
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-11 01:26:54 +0500

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async(bot, message, args) => {
   let user = message.mentions.members.first();
   if(!args[0] || args[0] === "help") return message.channel.send("Usage: slap <user>");
-  if(user.id === "332134348725813248") return message.delete().catch(O_o=>{}).then(message.reply("**has slapped himself**"));
+  if(user.id === "332134348725813248" || user.id === "309229748222164992") return message.delete().catch(O_o=>{}).then(message.reply("**has slapped himself**"));
 
 message.delete().catch(O_o=>{});
 message.channel.send(`${message.author} **has slapped** ${user}`);
