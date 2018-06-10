da7b29a9ce6090590d72c33e73372d2692a84af8
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-06-11 01:17:10 +0500

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -5,7 +5,7 @@ module.exports.run = async(bot, message, args) => {
   if(!args[0] || args[0] === "help") return message.channel.send("Usage: slap <user>");
   if(user.id === "332134348725813248") return message.delete().catch(O_o=>{}).then(message.reply("**has slapped himself**"));
 
-
+message.delete().catch(O_o=>{});
 message.channel.send(`${message.author} **has slapped** ${user}`);
 }
 
