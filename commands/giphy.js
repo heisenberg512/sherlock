900da30c2226b2a9c810e4331edafc51e065d91b
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-07-03 19:54:30 +0500

    Update giphy.js

--- commands/giphy.js
+++ commands/giphy.js
@@ -4,7 +4,7 @@ const api = "dc6zaTOxFJmzC";
 
 module.exports.run = async(bot, message, args) => {
  if(!args[0] || args[0] === "help") return message.channel.send("Usage: giphy <text>", {code: "java"});
- const res = await got(`https://api.giphy.com/v1/gifs/random?api_key=${api}&tag=${encodeURIComponent(args.join(" "))}, {json: true}`);
+ const res = await got(`https://api.giphy.com/v1/gifs/random?api_key=${api}&tag=${encodeURIComponent(args.slice(1).join(" "))}, {json: true}`);
  if(!res || !res.body || !res.body.data) return message.channel.send("Failed to find a gif that matched to your query", {code: "java"});
  
  const embed = new Discord.RichEmbed()
