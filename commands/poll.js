e4c7b607a454848a05df0785568783aeda8fd1ec
heisenberg512 <39162506+heisenberg512@users.noreply.github.com>
2018-07-10 12:03:09 +0500

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -4,7 +4,7 @@ const botconfig = require("../botconfig.json");
 module.exports.run = async (bot, message, args, tools) => {
   let text = args.join(" ");
   if(!args[0]) return message.channel.send("Usage: poll <question>", {code:'py'});
-  if(!message.member.hasPermission("ADMINISTRATOR")) return message.delete().catch(O_o=>{});
+  if(!message.member.hasPermission("ADMINISTRATOR")) return;
   
   let embed = new Discord.RichEmbed()
   .setTitle(`**Poll created by ${message.author.username}**`)
@@ -15,6 +15,7 @@ module.exports.run = async (bot, message, args, tools) => {
   let msg = await message.channel.send(embed);
   await msg.react('✅');
   await msg.react('❌');
+  if(message.member.reacts('✅')) return message.channel.send("your vote has been casted");
   message.delete().catch(O_o=>{});
 }
 
